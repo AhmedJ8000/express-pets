2f1e3b1c13f0bcf2d40fba299d5926ec2a7b1961
AhmedJ8000 <ahmedjamal8000@gmail.com>
2025-12-28 19:03:39 +0300

    Added delete for pets

--- controllers/pets.js
+++ controllers/pets.js
@@ -26,9 +26,48 @@ router.post('/', async (req, res) => {
 // GET + /pets/
 router.get('/', async (req, res) => {
     try {
-        res.json('success');
+        const pets = await Pet.find();
+        res.status(200).json({ pets })
     } catch (err) {
+        console.log(err);
+        res.status(500).json({ err: "Failed to get pets" });
+    }
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        // Get the id from the req.params
+        const { id } = req.params
+        // Use the model to find by id
+        const pet = await Pet.findById(id);
+        // If we don't get a pet, respond with 404
+        if (!pet) {
+            res.status(404).json({ error: "Pet not found" });
+        } else {
+            // Else: Send 200 with pet
+            res.status(200).json({ pet });
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err: "Failed to get pet" });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const pet = await Pet.findByIdAndDelete(id);
+
+        if (!pet) {
+            return res.status(404).json({ err: "Pet not found" });
+        }
+
+        res.status(200).json({ message: "Pet deleted successfully", pet });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ err: "Failed to delete pet" });
     }
 });
 
